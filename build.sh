0f32b96725b9684a861f6891e60980adec7e7577
Piotr Kono <piotr.konopacki@gmail.com>
2021-12-09 22:46:45 +0100

    apply fixes from shell check

--- build.sh
+++ build.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
-SCRIPT=`realpath $0`
-BASEDIR=`dirname $SCRIPT`
+SCRIPT=$(realpath $0)
+BASEDIR=$(dirname $SCRIPT)
 VERSION=0.3.26
 
 copy_files() {
-    mkdir -p $DEST
-    cp -R $BASEDIR/src      $DEST
-    cp -R $BASEDIR/assets   $DEST
-    cp -R $BASEDIR/LICENSE  $DEST
-    cp platform/$BROWSER/manifest.json $DEST
+    mkdir -p "$DEST"
+    cp -R "$BASEDIR"/src      "$DEST"
+    cp -R "$BASEDIR"/assets   "$DEST"
+    cp -R "$BASEDIR"/LICENSE  "$DEST"
+    cp platform/"$BROWSER"/manifest.json "$DEST"
 }
 
 set_version() {
-    sed -i -e "s/{EXT_VERSION}/${VERSION}/" ${DEST}/manifest.json
-    sed -i -e "s/{EXT_VERSION}/${VERSION}/" ${DEST}/src/ui/options.html
+    sed -i -e "s/{EXT_VERSION}/${VERSION}/" "${DEST}"/manifest.json
+    sed -i -e "s/{EXT_VERSION}/${VERSION}/" "${DEST}"/src/ui/options.html
 }
 
 uglify() {
-    pushd $DEST/src/scripts
+    pushd "$DEST"/src/scripts || exit
     terser --ecma 8 -o seed_.js seed.js
     sed -i -e "s/{SEED_CONTENTS}/$(sed 's:[/\\&]:\\&:g' seed_.js)/" content_script.js
     rm -f seed*.js;
 }
 
 zipfile() {
-    cd $DEST
+    cd "$DEST" || exit
     zip "$DEST/blocktube_$BROWSER_$VERSION.zip" -qr ./*
 }
 
 clean() {
-    rm -rf $DEST
+    rm -rf "$DEST"
 }
 
 build() {
